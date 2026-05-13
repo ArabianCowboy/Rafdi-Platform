ccf600fe118c8ec429fa84051db355fe57001cdc
NSF9 <alzhraniii999@gmail.com>
2026-05-13 08:01:05 +0300

    feat: add Payment and Notification APIs, and include routers in main application

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import engine
-from app.models.Base_Model import Base, TimestampMixin
-from app.api.Auth_Api import router as auth_router
-
+from app.models.Base_Model import Base
 from app.models import User, Company, Role, User_Role, Warehouse, Booking, Payment, Notification
+
+from app.api.Auth_Api         import router as auth_router
+from app.api.WareHouse_Api    import router as warehouse_router
+from app.api.Booking_Api      import router as booking_router
+from app.api.Payment_Api      import router as payment_router
+from app.api.Notification_Api import router as notification_router
 
 app = FastAPI(title="Rafdi Platform")
 
@@ -12,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://www.rafdi.com",
-        "https://rafdi-platform-frontend-production.up.railway.app",
+        "https://rafdi.com",
         "http://localhost:5173",
     ],
     allow_credentials=True,
@@ -20,7 +24,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
+app.include_router(warehouse_router)
+app.include_router(booking_router)
+app.include_router(payment_router)
+app.include_router(notification_router)
+app.include_router(admin_router)
